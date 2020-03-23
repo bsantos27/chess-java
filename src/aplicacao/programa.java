4da22ef148be127d80460a9298313a960921a8a9
Bruno Santos <bcs.270@gmail.com>
2020-03-23 19:34:25 -0300

    Pieces

--- src/aplicacao/programa.java
+++ src/aplicacao/programa.java
@@ -1,12 +1,12 @@
 package aplicacao;
 
+import boadgame.Board;
 import boadgame.Position;
 
 public class Programa {
 
 	public static void main(String[] arg) {
 		
-		Position pos = new Position(3, 5);
-		System.out.println(pos);
+	Board board = new Board(8, 8);
 	}
 }

0a4d4f3ec7fd9d0a03eb04c347b03c7babae9886
Bruno Santos <bcs.270@gmail.com>
2020-03-23 20:33:46 -0300

    continuaçao1

---
+++ src/chess/ChessPiece.java
@@ -0,0 +1,18 @@
+package chess;
+
+import java.awt.Color;
+
+import boadgame.Board;
+import boadgame.Piece;
+
+public class ChessPiece extends Piece {
+
+	private Color color;
+
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
+	
+	
+}
